574b9b20aaa9e8b91f35557a68a63a10cfc233c0
JvWelzenis <83783857+JvWelzenis@users.noreply.github.com>
2021-06-13 16:01:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+/*const argumentLesson = function(color) {
+console.log("the wall has painted " + color);
+
+};
+
+nothing */
+
+const wallColor = function (color) {
+console.log('the big wall is painted ' + color);
+};
+
+
+
+const wall = ['blue, green, purple'];
